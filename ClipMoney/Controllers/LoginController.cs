1468caeb27eb108f2d709adbcad03d0b78ee4637
Fabricio Alan Blas <fabricioalanblas@gmail.com>
2020-11-16 21:33:14 -0300

    Login clase y generador, controller en proceso

---
+++ ClipMoney/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+using ClipMoney.Models;
+
+namespace ClipMoney.Controllers
+{
+    [AllowAnonymous]
+    [RoutePrefix("api/login")]
+    public class LoginController : ApiController
+    {
+        [HttpGet]
+        [Route("echoping")]
+        public IHttpActionResult EchoPing()
+        {
+            return Ok(true);
+        }
+
+        [HttpGet]
+        [Route("echouser")]
+        public IHttpActionResult EchoUser()
+        {
+            var identity = Thread.CurrentPrincipal.Identity;
+            return Ok($" IPrincipal-user: {identity.Name} - IsAuthenticated: {identity.IsAuthenticated}");
+        }
+        [HttpPost]
+        [Route("authenticate")]
+        public IHttpActionResult Authenticate(Login login)
+        {
+
+            if (login == null)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            GestorLogin lc = new GestorLogin();
+            
+
+            return Ok(lc.validar_login(login));
+        }
+    }
+}
---
+++ ClipMoney/Models/GestorLogin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ClipMoney.Models
+{
+    public class GestorLogin
+    {
+
+        private SqlConnection cnn = new SqlConnection(ConfigurationManager.ConnectionStrings["db_clip"].ToString());
+
+        public bool validar_login(Login lg)
+        {
+            bool conectado = false;
+            using (cnn)
+            {
+                cnn.Open();
+
+                SqlCommand cmd = new SqlCommand("login", cnn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@usuario", lg.Usuario);
+                cmd.Parameters.AddWithValue("@contraseña", lg.Contraseña);
+
+                SqlDataReader lectura = cmd.ExecuteReader();
+
+                if (lectura.Read())
+                {
+                    if (lectura.GetInt32(0) != 0) conectado = true;
+                }
+                cnn.Close();
+            }
+
+            return conectado;
+        }
+    }
+}
